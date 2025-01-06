f5663d4b406ea1ac4841e38b9aea29fea6b5bc47
Brian-Agent <kipronobrian99@gmail.com>
2025-01-06 09:19:53 +0300

    b b

--- bettips/api/urls.py
+++ bettips/api/urls.py
@@ -3,7 +3,7 @@ from . import views
 urlpatterns= [
 
     path('',views.home,name='home'),
-    path('',views.all_games,name='all_games')
+    path('all_games',views.all_games,name='all_games')
 
 
 ]
